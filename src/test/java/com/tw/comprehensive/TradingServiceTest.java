076a479a1744589968d2365531f7a870b695fd0f
GONGBR2 <gongbr2@oocl.com>
2021-06-16 19:41:23 +0800

    [TradingService] feature: should_verify_check_logNewTrade_method_when_call_createTrade_method

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,23 @@
 package com.tw.comprehensive;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 class TradingServiceTest {
+    @Test
+    void should_verify_check_logNewTrade_method_when_call_createTrade_method() {
+        // given
+        TradeRepository tradeRepository = mock(TradeRepository.class);
+        AuditService auditService = spy(AuditService.class);
+        Trade trade = mock(Trade.class);
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+        // when
+        when(tradeRepository.createTrade(trade)).thenReturn(anyLong());
+        tradingService.createTrade(trade);
+        // then
+        verify(auditService, times(1)).logNewTrade(trade);
+    }
 
 }
